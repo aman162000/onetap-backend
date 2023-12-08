ec857f10743904970e297c099039e51b99c4e001
Aman <borseaman16@gmail.com>
2023-12-08 19:02:18 +0530

    update `asgi.py`

--- onetap/asgi.py
+++ onetap/asgi.py
@@ -7,16 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
-import os
-
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from beacon.middleware import JwtAuthMiddlewareStack
-from channels.security.websocket import AllowedHostsOriginValidator
 from beacon.routing import websocket_urlpatterns
+from channels.security.websocket import AllowedHostsOriginValidator
+from beacon.middleware import JwtAuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+import os
+import django
+django_asgi_app = get_asgi_application()
+django.setup()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onetap.settings')
 
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
