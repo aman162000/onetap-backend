8d9dcf276658ffa9cd40bd487346aabaee65d2f5
Aman <borseaman16@gmail.com>
2023-12-08 19:07:49 +0530

    update `asgi.py`

--- onetap/asgi.py
+++ onetap/asgi.py
@@ -5,18 +5,26 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+
 """
+
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onetap.settings')
+
+django.setup()
+
+
+
 
 from beacon.routing import websocket_urlpatterns
 from channels.security.websocket import AllowedHostsOriginValidator
 from beacon.middleware import JwtAuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import os
-import django
 django_asgi_app = get_asgi_application()
-django.setup()
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onetap.settings')
+
 
 
 application = ProtocolTypeRouter(
